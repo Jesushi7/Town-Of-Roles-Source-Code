d42fcb2a438b1e5c053a469a6bb2cf644ca0cadb
Jesushi7 <jlucho980@gmail.com>
2023-01-23 13:45:22 -0500

    changed end screen

--- source/Patches/NeutralRoles/PyromaniacMod/Outro.cs
+++ source/Patches/NeutralRoles/PyromaniacMod/Outro.cs
@@ -20,7 +20,7 @@ namespace TownOfUs.NeutralRoles.PyromaniacMod
             foreach (var player in array) player.NameText().text = role.ColorString + player.NameText().text + "</color>";
             __instance.BackgroundBar.material.color = role.Color;
             var text = Object.Instantiate(__instance.WinText);
-            text.text = "Pyromaniac Wins!";
+            text.text = "Pyromaniac Torched everyone!";
             text.color = role.Color;
             var pos = __instance.WinText.transform.localPosition;
             pos.y = 1.5f;
